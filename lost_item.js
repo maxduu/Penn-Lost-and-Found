3910ed32cdd236ce8b3e60d37f963331952955f8
Kevin Chen <kevc528@seas.upenn.edu>
2020-03-19 18:04:50 -0500

    lost_item schema

---
+++ lost_item.js
@@ -0,0 +1,21 @@
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://.......');
+
+var Schema = mongoose.Schema;
+
+var lostItemSchema = new Schema({
+    id: {type: Number, required: true, unique: true},
+    posterId: {type: Number, required: true, unique: false},
+    category: {type: String, required: true, unique: false},
+    date: {type: Date, required: true, unique: false},
+    latitude: {type: Number, required: false, unique: false},
+    longitude: {type: Number, required: false, unique: false},
+    around: {type: String, required: false, unique: false},
+    description: {type: String, required: false, unique: false},
+    attachmentLoc: {type: String, required: false, unique: false},
+    additionalInfo: {type: String, required: false, unique: false}
+})
+
+module.exports = mongoose.model('lost_item', lostItemSchema);
