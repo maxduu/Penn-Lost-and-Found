6c58cd95673a3424ec26c605584c8f2c9e130e0e
Kevin Chen <kevc528@seas.upenn.edu>
2020-03-20 17:17:43 -0500

    AccessWebTask android

---
+++ AndroidApp/app/src/main/java/edu/upenn/cis350/androidapp/AccessWebTask.java
@@ -0,0 +1,43 @@
+package edu.upenn.cis350.androidapp;
+
+import java.net.*;
+import android.os.*;
+import java.util.*;
+import org.json.*;
+
+public class AccessWebTask extends AsyncTask<URL, String, String> {
+
+    protected String doInBackground(URL... urls){
+
+        try {
+            // get the first URL from the array
+            URL url = urls[0];
+            // create connection and send HTTP request
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            // read first line of data that is returned
+            Scanner in = new Scanner(url.openStream());
+            String msg = in.nextLine();
+
+            // use Android JSON library to parse JSON
+            JSONObject jo = new JSONObject(msg);
+            // assumes that JSON object contains a "name" field
+            String name = jo.getString("name");
+            // this will be passed to onPostExecute method
+            return name;
+
+        }
+        catch (Exception e) {
+            return e.toString();
+        }
+
+    }
+
+    // called after doInBackground finishes
+    protected void onPostExecute(String msg) {
+
+    }
+
+}
