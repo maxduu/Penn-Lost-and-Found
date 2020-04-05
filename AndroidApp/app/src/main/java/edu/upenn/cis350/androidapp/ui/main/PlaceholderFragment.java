b692cb71c46e119e04d205037f07f77282845a6e
noxydude <60411452+noxydude@users.noreply.github.com>
2020-04-05 01:39:45 -0400

    final fixes

--- AndroidApp/app/src/main/java/edu/upenn/cis350/androidapp/ui/main/PlaceholderFragment.java
+++ AndroidApp/app/src/main/java/edu/upenn/cis350/androidapp/ui/main/PlaceholderFragment.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
