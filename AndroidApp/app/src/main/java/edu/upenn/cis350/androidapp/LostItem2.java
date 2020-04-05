55ac4c18577d971509f4074493a469d6b8aad19b
adisb01 <51427812+adisb01@users.noreply.github.com>
2020-04-04 22:00:53 -0500

    minor UI changes

--- AndroidApp/app/src/main/java/edu/upenn/cis350/androidapp/LostItem2.java
+++ AndroidApp/app/src/main/java/edu/upenn/cis350/androidapp/LostItem2.java
@@ -1,19 +1,10 @@
 package edu.upenn.cis350.androidapp;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.widget.TextView;
 
-import java.io.Serializable;
-import java.text.Format;
-import java.text.SimpleDateFormat;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LostItem2 extends AppCompatActivity {
 
@@ -29,7 +20,7 @@ public class LostItem2 extends AppCompatActivity {
         lostItemAround.setText(getIntent().getStringExtra("location"));
     }
 
-    public void onSendMessageClick (View v) {
+    public void onLostItemMessageClick (View v) {
 
     }
 
--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -16,6 +16,7 @@ router.use('/post', async (req, res) => {
     }); 
     try {
         const savedChat = await newChat.save()
+        res.json({'status': 'success'});
     } catch (err) {
         res.json({ 'message': err }); 
     } 
